111e4a5e0e162e55b8f1cdf6cd231efccb80bc7b
hyuk89101 <git@khs871004>
2023-03-22 13:47:35 +0900

    COMMIT

--- src/main/java/com/example/service2/controller/ApiTwoController.java
+++ src/main/java/com/example/service2/controller/ApiTwoController.java
@@ -2,6 +2,7 @@ package com.example.service2.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,13 @@ public class ApiTwoController {
     @GetMapping(value = "/openmarket/main")
     public String services() {
         return String.format("openmarket/main API Port : %s", "8012");
+    }
+
+    // message 메서드 추가
+    @GetMapping("/openmarket/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Service2.";
     }
 
 }
